4e36fa28ad7ac9d26304deb1bb8fdfda270fdb1d
princekumar111 <princekumar060420@gmail.com>
2026-06-11 15:47:28 +0530

    remove optional chaining from axios

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,32 +1,33 @@
-// src/utils/axios.js
-
 import axios from "axios";
 
 const API = axios.create({
     baseURL: "https://nearbyhelper-backend.onrender.com/api",
 });
 
-// Automatically attach token
 API.interceptors.request.use(
-    (config) => {
+    function(config) {
         const token = localStorage.getItem("token");
 
         if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
+            config.headers.Authorization = "Bearer " + token;
         }
 
         return config;
     },
-    (error) => {
+
+    function(error) {
         return Promise.reject(error);
     }
 );
 
-// Catch JWT expiration
-API.interceptors.response.use(
-    (response) => response,
 
-    (error) => {
+API.interceptors.response.use(
+    function(response) {
+        return response;
+    },
+
+    function(error) {
+
         if (
             error.response &&
             error.response.data &&
