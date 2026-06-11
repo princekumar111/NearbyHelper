e3eba485fbe854ba99e13629a5be3a848deded52
princekumar111 <princekumar060420@gmail.com>
2026-06-11 15:17:17 +0530

    update backend api url for deployment

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,26 +2,26 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:4000/api', // or your deployed URL
+    baseURL: 'https://nearbyhelper-backend.onrender.com/api', // or your deployed URL
 });
 
 // Automatically attach token to requests
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+    const token = localStorage.getItem('token');
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
 });
 
 // Catch JWT expiration globally
 API.interceptors.response.use(
-  res => res,
-  err => {
-    if (err.response?.data?.msg === 'jwt expired') {
-      localStorage.removeItem('token');
-      window.location.href = '/login';
+    res => res,
+    err => {
+        if (err.response ? .data ? .msg === 'jwt expired') {
+            localStorage.removeItem('token');
+            window.location.href = '/login';
+        }
+        return Promise.reject(err);
     }
-    return Promise.reject(err);
-  }
 );
 
 export default API;
