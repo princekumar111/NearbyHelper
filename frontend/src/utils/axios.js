3c78e73164caa172b4b1872f7c4856263076123b
princekumar111 <princekumar060420@gmail.com>
2026-06-11 15:28:02 +0530

    fix axios api url for vercel deployment

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,26 +1,42 @@
 // src/utils/axios.js
-import axios from 'axios';
+
+import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'https://nearbyhelper-backend.onrender.com/api', // or your deployed URL
+    baseURL: "https://nearbyhelper-backend.onrender.com/api",
 });
 
-// Automatically attach token to requests
-API.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-});
+// Automatically attach token
+API.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
 
-// Catch JWT expiration globally
-API.interceptors.response.use(
-    res => res,
-    err => {
-        if (err.response ? .data ? .msg === 'jwt expired') {
-            localStorage.removeItem('token');
-            window.location.href = '/login';
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
         }
-        return Promise.reject(err);
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+// Catch JWT expiration
+API.interceptors.response.use(
+    (response) => response,
+
+    (error) => {
+        if (
+            error.response &&
+            error.response.data &&
+            error.response.data.msg === "jwt expired"
+        ) {
+            localStorage.removeItem("token");
+            window.location.href = "/login";
+        }
+
+        return Promise.reject(error);
     }
 );
 
